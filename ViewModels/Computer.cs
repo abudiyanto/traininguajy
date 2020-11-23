7debffb7739bd7a0e5cff6a5d8d2954d66258632
dayankhatami_ <dayankhatamia@gmail.com>
2020-11-23 21:26:36 +0700

    Create Dropdown Brand and Post Add Computer

---
+++ ViewModels/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingUAJY.ViewModels
+{
+    public class Computer
+    {
+        public string SKU { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+    }
+    public class Brand
+    {
+        public string IdBrand { get; set; }
+        public string Title { get; set; }
+    }
+}

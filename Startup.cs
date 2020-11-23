5e7f14f97f866cd5e3d6658ca4e3d71b6b093e2c
cloudcomputingID <alex@cloudcomputing.id>
2020-11-23 19:35:42 +0700

    Initial Training 23 November 2020 - Day 1

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrainingUAJY.Startup))]
+namespace TrainingUAJY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
